c1b55aff94cc0344388c99f7451e9b1aa4714935
Viachaslau Latushkin <viachaslau.latushkin@gmail.com>
2017-02-05 02:11:30 +0300

    guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.result = 0;
+    	return this;
+	}
 
     setRange(min, max) {
-
+		this.min = min;
+		this.max = max;
+		this.result =  Math.ceil(this.max / 2);
+		return this;
     }
 
     guess() {
-
+    	return this.result;
     }
 
     lower() {
-
+    	this.max = this.result;
+    	this.result = Math.ceil((this.min + this.max) / 2);
     }
 
     greater() {
-
+    	this.min = this.result;
+		this.result = Math.ceil((this.min + this.max) / 2);
     }
 }
 
